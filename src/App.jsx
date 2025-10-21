dd26688ede2231c573ef602ca5a6129059d699ec
AvusalaChetan <avusalachetan@gmail.com>
2025-10-21 20:59:54 +0530

    app.jsx changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,41 @@
-import { useEffect, useState, lazy, Suspense } from "react";
-import Loading from "./components/Loading";
 import "./App.css";
-import {  AnimatePresence } from "motion/react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/homePage/Home";
-import About from "./pages/AboutPage/About";
-import Skills from "./pages/SkillsPage/Skills";
-import Projects from "./pages/ProjectPage/Projects";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import SkillsPage from "./pages/SkillsPage";
+import Cursor from "./animations/Cursor";
+import Projects from "./pages/Projects";
+import ScrollBar from "./animations/ScrollBar";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3100);
-
-    return () => clearTimeout(timer);
-  }, []);
-
-//lazy loading
-const LazyContactPage = lazy(()=>import('./pages/ContactPage/Contact'))
-
-  if (isLoading) {
-    return <Loading />
-  }
-
+  
+  const router = createBrowserRouter(
+    [
+      {
+        path: "",
+        element: (
+          <>
+            <Navbar />
+          </>
+        ),
+      },
+    ],
+    { future: { v7_startTransition: true } }
+  );
 
   return (
-    <>
-      <main
-        className="bg-gradient-to-br from-[#151042] via-[#302b63] to-[#24243e] text-white min-h-screen w-full overflow-x-hidden "
-      >
-        <Suspense>
-          <div className="loading-container">
-            <AnimatePresence>
-                <div key="navbar">
-                  <Navbar />
-                </div>
-              <div key="main-content" className="main-container w-full max-w-full px-2 sm:px-4 mx-auto">
-                <div className="w-full lg:w-[80vw] sm:m-auto  rounded-md bg-opacity-80"
-                  id="page-container"
-                >
-                  <div id="home" className="h-[80vh] w-[90vw] sm:m-none lg:w-[80vw]  flex items-center justify-center" >
-                    <Home />
-                  </div>
-                  <div id="about">
-                    <About />
-                  </div>
-                  <div id="skills" className="mt-6  w-[90vw]  ">
-                    <Skills />
-                  </div>
-                  <div id="projects">
-                    <Projects />
-                  </div>
-                  <div id="contact">
-                    <Suspense fallback={<div className="text-white">Loading contact...</div>}>
-                      <LazyContactPage />
-                    </Suspense>
-                  </div>
-                </div>
-              </div>
-            </AnimatePresence>
-          </div>
-        </Suspense>
-      </main>
-    </>
+    <main className="bg-white  w-full overflow-x-hidden ">
+    <ScrollBar>
+      <RouterProvider router={router}/>
+      <Cursor/>
+      <HomePage/>
+      <AboutPage/>
+      <SkillsPage/>
+      <Projects/>
+    </ScrollBar>
+    </main>
   );
 }
-
 export default App;
